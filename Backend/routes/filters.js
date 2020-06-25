b6aaa58162b33a52ed40f46887f17379c4308c12
carban <murillo.carlos@correounivalle.edu.co>
2020-06-25 15:41:55 -0500

    improve

--- Backend/routes/filters.js
+++ Backend/routes/filters.js
@@ -119,94 +119,6 @@ function extractFiltersOnMap(array) {
   return { departamentos, municipios, fincas };
 }
 
-
-
-// ------------------------------------------------------------------------------------
-
-// ||||||||||||||||||||||| Ruta ||||||||||||||||||||||| 
-// Retorna toda la informacion de los filtros que esten almacenados en la BD
-router.get('/', async (req, res) => {
-
-  const catsubs = {
-    text: "select * from categoria inner join subcategoria on categoria_idcategoria = idcategoria order by idcategoria;",
-  }
-
-  const municipios = {
-    text: "select idmunicipio, municipio from municipio;"
-  }
-
-  const tipos = {
-    text: "select DISTINCT ON(tipo) tipo from metadato;"
-  }
-
-  const formatos = {
-    text: "select DISTINCT ON(formato) formato from metadato;"
-  }
-
-  const fincas = {
-    text: "select municipio_idmunicipio, finca from finca order by 1;"
-  }
-
-  try {
-    const result_catsubs = await pg.query(catsubs);
-    const result_municipios = await pg.query(municipios);
-    const result_tipos = await pg.query(tipos);
-    const result_formatos = await pg.query(formatos);
-    const result_fincas = await pg.query(fincas);
-
-    var cats = processCats(result_catsubs.rows);
-    var subs = processSubs(result_catsubs.rows);
-
-    // console.log(result_municipios.rows);
-    // console.log(result_fincas.rows);
-
-    var munis = processMunicipios(result_municipios.rows);
-    var fins = processFincas(result_fincas.rows);
-    // console.log(munis, fins);
-
-    res.status(200).send({ cats: cats, subs: subs, municipios: munis, tipos: result_tipos.rows, formatos: result_formatos.rows, fincas: fins });
-  } catch (e) {
-    console.log(e);
-    res.sendStatus(400);
-  }
-})
-
-// ||||||||||||||||||||||| Ruta ||||||||||||||||||||||| 
-// Retorna toda la informacion de los filtros que esten almacenados en la BD
-// CORRESPONDIENTES a la hora de consultar en el mapa, 
-// seleccionando y buscando al dar click en un elemento del mapa.
-// En este caso se omite la consulta de finca y municipio
-router.get('/ubication', async (req, res) => {
-
-  const catsubs = {
-    text: "select * from categoria inner join subcategoria on categoria_idcategoria = idcategoria order by idcategoria;",
-  }
-
-  const tipos = {
-    text: "select DISTINCT ON(tipo) tipo from metadato;"
-  }
-
-  const formatos = {
-    text: "select DISTINCT ON(formato) formato from metadato;"
-  }
-
-
-  try {
-    const result_catsubs = await pg.query(catsubs);
-    const result_tipos = await pg.query(tipos);
-    const result_formatos = await pg.query(formatos);
-
-    var cats = processCats(result_catsubs.rows);
-    var subs = processSubs(result_catsubs.rows);
-
-    res.status(200).send({ cats: cats, subs: subs, tipos: result_tipos.rows, formatos: result_formatos.rows });
-  } catch (e) {
-    console.log(e);
-    res.sendStatus(400);
-  }
-})
-
-
 function getHashCultivo_Departamento(array) {
 
   hash = {};
@@ -295,6 +207,93 @@ function getCultivoList(obj) {
   return array;
 }
 
+
+
+// ------------------------------------------------------------------------------------
+
+// ||||||||||||||||||||||| Ruta ||||||||||||||||||||||| 
+// Retorna toda la informacion de los filtros que esten almacenados en la BD
+router.get('/', async (req, res) => {
+
+  const catsubs = {
+    text: "select * from categoria inner join subcategoria on categoria_idcategoria = idcategoria order by idcategoria;",
+  }
+
+  const municipios = {
+    text: "select idmunicipio, municipio from municipio;"
+  }
+
+  const tipos = {
+    text: "select DISTINCT ON(tipo) tipo from metadato;"
+  }
+
+  const formatos = {
+    text: "select DISTINCT ON(formato) formato from metadato;"
+  }
+
+  const fincas = {
+    text: "select municipio_idmunicipio, finca from finca order by 1;"
+  }
+
+  try {
+    const result_catsubs = await pg.query(catsubs);
+    const result_municipios = await pg.query(municipios);
+    const result_tipos = await pg.query(tipos);
+    const result_formatos = await pg.query(formatos);
+    const result_fincas = await pg.query(fincas);
+
+    var cats = processCats(result_catsubs.rows);
+    var subs = processSubs(result_catsubs.rows);
+
+    // console.log(result_municipios.rows);
+    // console.log(result_fincas.rows);
+
+    var munis = processMunicipios(result_municipios.rows);
+    var fins = processFincas(result_fincas.rows);
+    // console.log(munis, fins);
+
+    res.status(200).send({ cats: cats, subs: subs, municipios: munis, tipos: result_tipos.rows, formatos: result_formatos.rows, fincas: fins });
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(400);
+  }
+})
+
+// ||||||||||||||||||||||| Ruta ||||||||||||||||||||||| 
+// Retorna toda la informacion de los filtros que esten almacenados en la BD
+// CORRESPONDIENTES a la hora de consultar en el mapa, 
+// seleccionando y buscando al dar click en un elemento del mapa.
+// En este caso se omite la consulta de finca y municipio
+router.get('/ubication', async (req, res) => {
+
+  const catsubs = {
+    text: "select * from categoria inner join subcategoria on categoria_idcategoria = idcategoria order by idcategoria;",
+  }
+
+  const tipos = {
+    text: "select DISTINCT ON(tipo) tipo from metadato;"
+  }
+
+  const formatos = {
+    text: "select DISTINCT ON(formato) formato from metadato;"
+  }
+
+
+  try {
+    const result_catsubs = await pg.query(catsubs);
+    const result_tipos = await pg.query(tipos);
+    const result_formatos = await pg.query(formatos);
+
+    var cats = processCats(result_catsubs.rows);
+    var subs = processSubs(result_catsubs.rows);
+
+    res.status(200).send({ cats: cats, subs: subs, tipos: result_tipos.rows, formatos: result_formatos.rows });
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(400);
+  }
+})
+
 // ||||||||||||||||||||||| Ruta ||||||||||||||||||||||| 
 // Retorna la informacion de departamentos, municipios y fincas correspondies,
 // para los filtros de poligonos en el mapa
@@ -313,6 +312,7 @@ router.get('/onmap', async (req, res) => {
   var hash_Muni = getHashDepartamento_Municipio(result2.rows);
   var hash_Fin = getHashMunicipio_Finca(result3.rows);
   var cultivo_list = getCultivoList(hash_Dept);
+
   res.json({
     cultivos: cultivo_list,
     departamentos: hash_Dept,
